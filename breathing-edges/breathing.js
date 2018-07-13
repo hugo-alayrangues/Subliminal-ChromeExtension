fe40118c2e1846a459ab2d85d97b3441fdd79471
Jonathan Burkle <jonathan.k.burkle@gmail.com>
2018-07-13 11:48:58 -0700

    Integrate color changing option

--- breathing-edges/breathing.js
+++ breathing-edges/breathing.js
@@ -1,14 +1,56 @@
 // chrome.runtime.sendMessage({todo: "showPageAction"});
+
+// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+//     if (changeInfo.status == 'complete') {
+//         update();
+//     }
+// });
+
+var sheet = document.createElement('style');
+document.body.appendChild(sheet);
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.todo == "enableBox")
+    if (request.todo == "update")
     {
-        var div=document.createElement("div"); 
-        div.setAttribute("id", "breathebox"); 
-        document.body.insertBefore(div, document.body.firstChild);
-    }
-    else if (request.todo == "disableBox")
-    {
-        var div=document.getElementById("breathebox"); 
-        div.remove();
+        update();
     }
 });
+
+function update()
+{
+    chrome.storage.sync.get({
+        enabled: false,
+        color: ""
+    }, function(items) {
+        var divs = document.body.querySelectorAll("#breathebox");
+        if (divs.length < 1 && items.enabled)
+        {
+            var div = document.createElement("div"); 
+            div.setAttribute("id", "breathebox"); 
+            document.body.insertBefore(div, document.body.firstChild);
+        }
+        else if (divs.length > 0 && !items.enabled)
+        {
+            var div = document.getElementById("breathebox"); 
+            div.remove();
+        }
+
+        if (items.color != "")
+        {
+            sheet.innerHTML = "@keyframes breathe { \n" +
+                              "0% { box-shadow: \n" +
+                              "inset 10px 10px 40px "   + items.color + ", \n" +
+                              "inset -10px -10px 40px " + items.color + "; } \n" +
+
+                              "50% { box-shadow: \n" +
+                              "inset 0px 0px 80px "   + items.color + ", \n" +
+                              "inset -0px -0px 80px " + items.color + "; } \n" +
+
+                              "100% { box-shadow: \n" +
+                              "inset 10px 10px 40px "   + items.color + ", \n" +
+                              "inset -10px -10px 40px " + items.color + "; } \n" +
+
+                              "}";
+        }
+    });
+}
--- breathing-edges/popup.js
+++ breathing-edges/popup.js
@@ -5,51 +5,85 @@ function checkEnable(e)
     chrome.storage.sync.set({
         enabled: this.checked
     }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
+        update_status();
     });
 
-    set_breathe();
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
+    });
 }
 
-function set_breathe()
+function changeHex(e)
 {
-    chrome.storage.sync.get({
-        enabled: false
-    }, function(items) {
-        if (items.enabled)
+    var reg = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/;
+    if (reg.exec(this.value))
+    {
+        var hex_val = this.value;
+        if (hex_val.charAt(0) != '#') // fix string if needed
         {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {todo: "enableBox"});
-            });
+            hex_val = '#' + hex_val;
         }
-        else
-        {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {todo: "disableBox"});
-            });
-        }
-    });
+
+        document.getElementById("colorBox").style.background = hex_val;
+        document.querySelector(".slider").style.background = hex_val;
+        // sheet.innerHTML = "input:checked + .slider {background-color: #" + this.value + ";}";
+
+        chrome.storage.sync.set({
+            color: hex_val
+        }, function() {
+            update_status();
+        });
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
+        });
+    }
 }
 
 function restore_options() 
 {
     // Defaults to disabled
     chrome.storage.sync.get({
-        enabled: false
+        enabled: false,
+        color: ""
     }, function(items) {
         document.querySelector("input[type=checkbox]").checked = items.enabled;
+        document.getElementById("colorBox").value = items.color;
+
+        // update styles
+        document.getElementById("colorBox").style.background = items.color;
+        document.querySelector(".slider").style.background = items.color;
     });
 }
+
+function update_status()
+{
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+
+    setTimeout(function() {
+        status.textContent = '';
+    }, 750);
+}
+
+// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+//     if (changeInfo.status == 'complete') {
+//         update();
+//     }
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
     restore_options();
     var checkbox = document.querySelector("input[type=checkbox]");
-    checkbox.addEventListener('change', checkEnable);
-    set_breathe();
+    checkbox.addEventListener('click', checkEnable);
+    var colorbox = document.getElementById("colorBox");
+    colorbox.addEventListener('keyup', changeHex);
+
+    // var sheet = document.createElement('style');
+    // document.appendChild(sheet);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
+    });
 });
 
