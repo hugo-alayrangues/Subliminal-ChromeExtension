b45996a2a6cb8cec6d1d1538ca1d6f1923b16ad0
Jonathan Burkle <jonathan.k.burkle@gmail.com>
2018-07-16 16:54:49 -0700

    Add opacity option

--- breathing-edges/breathing.js
+++ breathing-edges/breathing.js
@@ -9,6 +9,7 @@
 var sheet = document.createElement('style');
 document.body.appendChild(sheet);
 
+// process update message
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.todo == "update")
     {
@@ -16,13 +17,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
+// update all options
 function update()
 {
     chrome.storage.sync.get({
         enabled: false,
-        color: ""
+        color: "",
+        opacity: 1.0
     }, function(items) {
-        var divs = document.body.querySelectorAll("#breathebox");
+        var divs = document.body.querySelectorAll("#breathebox"); // check if there's already a box
         if (divs.length < 1 && items.enabled)
         {
             var div = document.createElement("div"); 
@@ -35,22 +38,33 @@ function update()
             div.remove();
         }
 
+        // update color and opacity
         if (items.color != "")
         {
             sheet.innerHTML = "@keyframes breathe { \n" +
                               "0% { box-shadow: \n" +
-                              "inset 10px 10px 40px "   + items.color + ", \n" +
-                              "inset -10px -10px 40px " + items.color + "; } \n" +
+                              "inset 10px 10px 40px "   + hex_to_rgba(items.color, items.opacity) + ", \n" +
+                              "inset -10px -10px 40px " + hex_to_rgba(items.color, items.opacity) + "; } \n" +
 
                               "50% { box-shadow: \n" +
-                              "inset 0px 0px 80px "   + items.color + ", \n" +
-                              "inset -0px -0px 80px " + items.color + "; } \n" +
+                              "inset 0px 0px 80px "   + hex_to_rgba(items.color, items.opacity) + ", \n" +
+                              "inset -0px -0px 80px " + hex_to_rgba(items.color, items.opacity) + "; } \n" +
 
                               "100% { box-shadow: \n" +
-                              "inset 10px 10px 40px "   + items.color + ", \n" +
-                              "inset -10px -10px 40px " + items.color + "; } \n" +
+                              "inset 10px 10px 40px "   + hex_to_rgba(items.color, items.opacity) + ", \n" +
+                              "inset -10px -10px 40px " + hex_to_rgba(items.color, items.opacity) + "; } \n" +
 
                               "}";
         }
     });
+}
+
+// helper function for updating color and opacity
+function hex_to_rgba(hex_val, opacity) 
+{
+    var r = parseInt(hex_val.slice(1, 3), 16);
+    var g = parseInt(hex_val.slice(3, 5), 16);
+    var b = parseInt(hex_val.slice(5, 7), 16);
+
+    return "rgba(" + r + ", " + g + ", " + b + ", " + opacity + ")";
 }
--- breathing-edges/popup.js
+++ breathing-edges/popup.js
@@ -1,22 +1,26 @@
 'use strict';
 
-function checkEnable(e)
+// update enable checkbox
+function check_enable(e)
 {
+    // store value
     chrome.storage.sync.set({
         enabled: this.checked
     }, function() {
         update_status();
     });
 
+    // update page
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
     });
 }
 
-function changeHex(e)
+// update color box
+function change_hex(e)
 {
     var reg = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/;
-    if (reg.exec(this.value))
+    if (reg.exec(this.value)) // check for valid text input
     {
         var hex_val = this.value;
         if (hex_val.charAt(0) != '#') // fix string if needed
@@ -24,41 +28,69 @@ function changeHex(e)
             hex_val = '#' + hex_val;
         }
 
+        // update styles accordingly
         document.getElementById("colorBox").style.background = hex_val;
         document.querySelector(".slider").style.background = hex_val;
-        // sheet.innerHTML = "input:checked + .slider {background-color: #" + this.value + ";}";
+        document.querySelector('#range-value-bar').style.background = hex_val;
 
+        // store value
         chrome.storage.sync.set({
             color: hex_val
         }, function() {
             update_status();
         });
 
+        // update page
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
         });
     }
 }
 
+// update opacity
+function change_opacity(e)
+{
+    // update styles
+    document.querySelector('#range-value-bar').style.setProperty('opacity', this.value/100);
+
+    // store value
+    chrome.storage.sync.set({
+        opacity: this.value/100
+    }, function() {
+        update_status();
+    });
+
+    // update page
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
+    });
+}
+
+// restore options when popup is opened
 function restore_options() 
 {
     // Defaults to disabled
     chrome.storage.sync.get({
         enabled: false,
-        color: ""
+        color: "",
+        opacity: 1.0
     }, function(items) {
+        // update values
         document.querySelector("input[type=checkbox]").checked = items.enabled;
         document.getElementById("colorBox").value = items.color;
+        document.querySelector("input[type=range]").value = items.opacity*100;
 
         // update styles
         document.getElementById("colorBox").style.background = items.color;
         document.querySelector(".slider").style.background = items.color;
+        document.querySelector('#range-value-bar').style.background = items.color;
+        document.querySelector('#range-value-bar').style.setProperty('opacity', items.opacity);
     });
 }
 
 function update_status()
 {
-    // Update status to let user know options were saved.
+    // update status to show options saved
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
 
@@ -73,15 +105,23 @@ function update_status()
 //     }
 // });
 
+// execute when popup loaded
 document.addEventListener('DOMContentLoaded', function () {
+    // restore previous options
     restore_options();
+    
+    // add listeners
     var checkbox = document.querySelector("input[type=checkbox]");
-    checkbox.addEventListener('click', checkEnable);
+    checkbox.addEventListener('click', check_enable);
     var colorbox = document.getElementById("colorBox");
-    colorbox.addEventListener('keyup', changeHex);
+    colorbox.addEventListener('input', change_hex);
+    var range_slider = document.querySelector('.range-slider');
+    range_slider.addEventListener('input', change_opacity);
 
     // var sheet = document.createElement('style');
     // document.appendChild(sheet);
+    
+    // initial update of page
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
     });
